0e0f2ab87da88bf3090d21a8da72df8747c564bc
ivan domnikov <ivan@haleytek.com>
2024-12-20 15:02:22 +0100

    TEST RUN: fre 20 dec 2024 15:02:22 CET

---
+++ 02/main.cpp
@@ -0,0 +1,47 @@
+#ifdef TEST
+#include "in_test.hpp"
+#else
+#include "in.hpp"
+#endif
+
+#include <cassert>
+#include <cmath>
+#include <queue>
+#include <list>
+#include <limits>
+
+auto in = getInput();
+
+auto count1() {
+    LL score = 0;
+    FOR(i, in.size()){
+        auto vec = splitStr(in[i], " ");
+        bool safe = true;
+        FOR(v,vec.size()){
+            if (v == 0){continue;}
+            if (vec[v-1] <= vec[v]) {
+                safe = false;
+                break;
+            }
+        }
+        score += safe;
+    }
+    return score;
+}
+
+auto count2() {
+    LL score = 0;
+    return score;
+}
+
+int main(int argc, char** argv)
+{
+    LL score = 0;
+    score = count1();
+    P_RR("Part1: %lld\n", score);
+    //========================================================
+
+    score = count2();
+    P_RR("Part2: %lld\n", score);
+    return 0;
+}
