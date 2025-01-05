fa144eb1e56512e63b0eba5492c704d4034cc4eb
ivan domnikov <ivan@haleytek.com>
2025-01-05 18:55:55 +0100

    TEST RUN: sön  5 jan 2025 18:55:55 CET

--- 03/main.cpp
+++ 03/main.cpp
@@ -38,7 +38,7 @@ auto count1() {
 auto count2() {
     LL score = 0;
     auto s = in[0];
-    std::regex word_regex("mul\\([0-9]+,[0-9]+\\)|do()|don't()");
+    std::regex word_regex("mul\\([0-9]+,[0-9]+\\)|do|don't()");
     auto words_begin = std::sregex_iterator(s.begin(), s.end(), word_regex);
     auto words_end = std::sregex_iterator();
     for (std::sregex_iterator i = words_begin; i != words_end; ++i)
